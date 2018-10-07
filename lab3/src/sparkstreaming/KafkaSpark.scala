f71817ba5d70eb6d6176c38e7633553a3f824c3d
Miquel Larsson <miquellarsson@gmail.com>
2018-10-07 16:24:08 +0200

    Work on kafkaSpark code, mapping function is missing

--- lab3/src/sparkstreaming/KafkaSpark.scala
+++ lab3/src/sparkstreaming/KafkaSpark.scala
@@ -21,24 +21,38 @@ import com.datastax.spark.connector.streaming._
 object KafkaSpark {
   def main(args: Array[String]) {
     // connect to Cassandra and make a keyspace and table as explained in the document
-    val cluster = Cluster.<FILL IN>
+    val cluster = Cluster.builder().addContactPoint("localhost").build()
     val session = cluster.connect()
-    session.execute(.<FILL IN>)
+    session.execute("CREATE KEYSPACE IF NOT EXISTS avg_space WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };")
+    session.execute("CREATE TABLE IF NOT EXISTS avg_space.avg (word text PRIMARY KEY, count float);")
 
     // make a connection to Kafka and read (key, value) pairs from it
     <FILL IN>
-    val kafkaConf = <FILL IN>
-    val messages = KafkaUtils.createDirectStream.<FILL IN>
-    <FILL IN>
+    val kafkaConf = Map(
+      "metadata.broker.list" -> "localhost:9092",
+      "zookeeper.connect" -> "localhost:2181",
+      "group.id" -> "kafka-spark-streaming",
+      "zookeeper.connection.timeout.ms" -> "1000")
+    //val kafkaConf = Map[String, String]("metadata.broker.list" -> "localhost:9092")
+
+    val ssc = new StreamingContext(conf, Seconds(1))
+    val topicsSet = "avg".split(",").toSet
+
+    //val messages = KafkaUtils.createDirectStream.<FILL IN>
+    val kafkaStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
+                      ssc, kafkaConf, topicsSet)
+
+    //<FILL IN>
 
     // measure the average value for each key in a stateful manner
     def mappingFunc(key: String, value: Option[Double], state: State[Double]): (String, Double) = {
-	<FILL IN>
+	    //<FILL IN>
+
     }
     val stateDstream = pairs.mapWithState(<FILL IN>)
 
     // store the result in Cassandra
-    stateDstream.<FILL IN>
+    stateDstream.saveToCassandra("test", "words", SomeColumns("word", "count")
 
     ssc.start()
     ssc.awaitTermination()
