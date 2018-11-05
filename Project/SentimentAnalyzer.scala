bbab0340aa846eac052b9d22768c390ce31e2e67
Dmytro Siniukov <dmytro.siniukov@schibsted.com>
2018-11-05 18:35:33 +0100

    SentimentAnalyzer string+int

--- Project/SentimentAnalyzer.scala
+++ Project/SentimentAnalyzer.scala
@@ -13,15 +13,16 @@ object SentimentAnalyzer {
   props.setProperty("annotators", "tokenize, ssplit, parse, sentiment")
   val pipeline: StanfordCoreNLP = new StanfordCoreNLP(props)
 
-  def mainSentiment(input: String): Int = Option(input) match {
+  def mainSentiment(input: String): (Int, String) = Option(input) match {
     case Some(text) if !text.isEmpty => extractSentiment(text)
     case _ => throw new IllegalArgumentException("input can't be null or empty")
   }
 
-  def extractSentiment(text: String): Int = {
+  def extractSentiment(text: String): (Int, String) = {
     val (_, sentiment) = extractSentiments(text)
       .maxBy { case (sentence, _) => sentence.length }
-    sentiment
+    val stringSentiment = toSentiment(sentiment)
+    (sentiment, stringSentiment)
   }
 
   def extractSentiments(text: String): List[(String, Int)] = {
@@ -31,6 +32,12 @@ object SentimentAnalyzer {
       .map(sentence => (sentence, sentence.get(classOf[SentimentCoreAnnotations.SentimentAnnotatedTree])))
       .map { case (sentence, tree) => (sentence.toString,RNNCoreAnnotations.getPredictedClass(tree)) }
       .toList
+  }
+
+  def toSentiment(sentiment: Int): String = sentiment match {
+    case x if x == 0 || x == 1 => "NEGATIVE"
+    case 2 => "NEUTRAL"
+    case x if x == 3 || x == 4 => "POSITIVE"
   }
 
 }
